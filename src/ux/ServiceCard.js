fee2b80425b706cf97d2344c475233b91ec8b476
mlabato <m_labato@hotmail.com>
2023-04-13 17:55:43 -0300

    contact icon fixes

--- src/ux/ServiceCard.js
+++ src/ux/ServiceCard.js
@@ -5,7 +5,7 @@ const ServiceCard = (props) => {
   return (
     <article className={cardClasses}>
       <div className="w-[10rem] group-hover:w-full ease-in-out  h-full group-hover:bg-black duration-300 flex items-end">
-        <p className="group-hover:hidden  font-bold ml-[2rem]  notebook:ml-[1rem] py-[1rem] text-[1.8rem] text-white cursor-pointer">
+        <p className="group-hover:hidden  font-bold   notebook:ml-[1rem] py-[1rem] text-[1.8rem] text-white cursor-pointer">
           {props.title}
         </p>
         <p className="hidden group-hover:block  text-white font-semibold text-[1rem] notebook:text-[.8rem] big:text-[1rem] p-[2rem]">
